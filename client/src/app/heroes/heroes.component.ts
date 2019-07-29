07d28b17d37c65ad5b88e8fae82eeca67d1c4538
zhikang.zeng <zhikang.zeng@innocellence.com>
2019-07-29 13:59:19 +0800

    add search

--- client/src/app/heroes/heroes.component.ts
+++ client/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,14 +12,25 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
   constructor(
-    private heroSrevice: HeroService
+    private heroService: HeroService
   ) { }
 
   ngOnInit() {
     this.getHeroes();
   }
   getHeroes(): void {
-    this.heroSrevice.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
